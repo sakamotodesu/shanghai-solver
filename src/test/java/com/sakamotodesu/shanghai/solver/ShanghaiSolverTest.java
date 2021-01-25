307cb5cac65f792818761ad7dd5e14308af9483d
Minoru Sakamoto <p2020640@gmail.com>
2021-01-25 23:05:18 +0900

    殿堂入り入力中

--- src/test/java/com/sakamotodesu/shanghai/solver/ShanghaiSolverTest.java
+++ src/test/java/com/sakamotodesu/shanghai/solver/ShanghaiSolverTest.java
@@ -10,8 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.sakamotodesu.shanghai.solver.pitype.Manzu.*;
-import static com.sakamotodesu.shanghai.solver.pitype.Pinzu.ePin;
-import static com.sakamotodesu.shanghai.solver.pitype.Pinzu.ryanPin;
+import static com.sakamotodesu.shanghai.solver.pitype.Pinzu.*;
 import static com.sakamotodesu.shanghai.solver.pitype.Souzu.*;
 import static com.sakamotodesu.shanghai.solver.pitype.Zihai.*;
 import static org.hamcrest.CoreMatchers.is;
@@ -28,6 +27,7 @@ public class ShanghaiSolverTest {
     List<Pi> sixPiesOnlyOneWayPossible;
     List<Pi> twoFloor;
     List<Pi> normal;
+    List<Pi> dendo;
 
 
     @Before
@@ -126,6 +126,64 @@ public class ShanghaiSolverTest {
                 new PlacedPi(roSou, 12, 2, 1)
 
         ));
+
+        dendo = new ArrayList<>(Arrays.asList(new PlacedPi(nan, 0, 8, 0),
+                new PlacedPi(suPin, 0, 10, 0),
+                new PlacedPi(chiSou, 0, 12, 0),
+
+                new PlacedPi(roPin, 2, 5, 0),
+                new PlacedPi(haku, 2, 7, 0),
+                new PlacedPi(sha, 2, 9, 0),
+                new PlacedPi(ryu, 2, 11, 0),
+                new PlacedPi(suWan, 2, 13, 0),
+                new PlacedPi(sha, 2, 15, 0),
+
+                new PlacedPi(nan, 4, 4, 0),
+                new PlacedPi(season, 4, 6, 0),
+                new PlacedPi(quPin, 4, 8, 0),
+                new PlacedPi(chun, 4, 10, 0),
+                new PlacedPi(pe, 4, 12, 0),
+                new PlacedPi(chiSou, 4, 14, 0),
+                new PlacedPi(pe, 4, 16, 0),
+
+                new PlacedPi(eSou, 6, 2, 0),
+                new PlacedPi(suWan, 6, 4, 0),
+                new PlacedPi(ryanMan, 6, 6, 0),
+                new PlacedPi(ryanMan, 6, 8, 0),
+                new PlacedPi(haku, 6, 10, 0),
+                new PlacedPi(ryanPin, 6, 12, 0),
+                new PlacedPi(haku, 6, 14, 0),
+                new PlacedPi(roWan, 6, 16, 0),
+                new PlacedPi(quPin, 6, 18, 0),
+
+                new PlacedPi(pe, 7, 0, 0),
+                new PlacedPi(season, 7, 20, 0),
+
+                new PlacedPi(ton, 8, 2, 0),
+                new PlacedPi(ePin, 8, 4, 0),
+                new PlacedPi(chun, 8, 6, 0),
+                new PlacedPi(uSou, 8, 8, 0),
+                new PlacedPi(ton, 8, 10, 0),
+                new PlacedPi(chun, 8, 12, 0),
+                new PlacedPi(uSou, 8, 14, 0),
+                new PlacedPi(ton, 8, 16, 0),
+                new PlacedPi(eMan, 8, 18, 0),
+
+                new PlacedPi(sanWan, 9, 0, 0),
+                new PlacedPi(sanPin, 9, 20, 0),
+
+                new PlacedPi(pe, 10, 2, 0),
+                new PlacedPi(suWan, 10, 6, 0),
+                new PlacedPi(eMan, 10, 14, 0),
+                new PlacedPi(suPin, 10, 18, 0),
+
+                new PlacedPi(chun, 11, 4, 0),
+                new PlacedPi(quPin, 11, 8, 0),
+                new PlacedPi(ryanMan, 11, 10, 0),
+                new PlacedPi(suPin, 11, 12, 0),
+                new PlacedPi(eMan, 11, 16, 0),
+
+                new PlacedPi(ryanMan, 0, 2, 1)));
     }
 
     @Test
